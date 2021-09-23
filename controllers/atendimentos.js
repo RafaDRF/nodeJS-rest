3a4c2eab1608cc3256bd08c42d70d4f25b8e8404
Rafael Dutra <rafaeldutraribas@gmail.com>
2021-09-23 19:41:32 -0300

    feat posts creates new lines on data base

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota Atendimentos e realizou um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const conteudoPost = req.body
+
+        Atendimento.adiciona(conteudoPost)
+        
         res.send('Você está na rota Atendimentos e realizou um POST')
     })
 }
